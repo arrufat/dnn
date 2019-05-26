a5bdf5401c2dc5aea6eb85706ee8553caab84c83
Adrià Arrufat <adria.arrufat@outlook.com>
2019-05-27 00:26:27 +0900

    abstract the residual and residual down blocks

--- dnn/blocks.h
+++ dnn/blocks.h
@@ -41,49 +41,42 @@ namespace dnn
     >
     using residual_down = add_prev2<avg_pool<2, 2, 1, skip1<tag2<BLOCK<num_filters, BN, 2, tag1<SUBNET>>>>>>;
 
-    // residual block
+    // residual block with optional downsampling and batch normalization
     template<
+        template<template<int, template<typename> class, int, typename> class, int, template<typename>class, typename> class RESIDUAL,
         template<int, template<typename> class, int, typename> class BLOCK,
         int num_filters,
         template<typename> class BN,
         typename SUBNET
     >
-    using resblock = relu<residual<BLOCK, num_filters, BN, SUBNET>>;
-    // residual block
-    template<
-        template<int, template<typename> class, int, typename> class BLOCK,
-        int num_filters,
-        template<typename> class BN,
-        typename SUBNET
-    >
-    using resblock_down = relu<residual_down<BLOCK, num_filters, BN, SUBNET>>;
+    using residual_block = relu<RESIDUAL<BLOCK, num_filters, BN, SUBNET>>;
 
     template<int num_filters, typename SUBNET>
-    using resbasicblock_down = resblock_down<basicblock, num_filters, bn_con, SUBNET>;
+    using resbasicblock_down = residual_block<residual_down, basicblock, num_filters, bn_con, SUBNET>;
     template<int num_filters, typename SUBNET>
-    using resbottleneck_down = resblock_down<bottleneck, num_filters, affine, SUBNET>;
+    using resbottleneck_down = residual_block<residual_down, bottleneck, num_filters, affine, SUBNET>;
     template<int num_filters, typename SUBNET>
-    using aresbasicblock_down = resblock_down<basicblock, num_filters, affine, SUBNET>;
+    using aresbasicblock_down = residual_block<residual_down, basicblock, num_filters, affine, SUBNET>;
     template<int num_filters, typename SUBNET>
-    using aresbottleneck_down = resblock_down<bottleneck, num_filters, bn_con, SUBNET>;
+    using aresbottleneck_down = residual_block<residual_down, bottleneck, num_filters, bn_con, SUBNET>;
 
     // some useful definitions to allow the use of the repeat layer
-    template<typename SUBNET> using resbasicblock512 = resblock<basicblock, 512, bn_con, SUBNET>;
-    template<typename SUBNET> using resbasicblock256 = resblock<basicblock, 256, bn_con, SUBNET>;
-    template<typename SUBNET> using resbasicblock128 = resblock<basicblock, 128, bn_con, SUBNET>;
-    template<typename SUBNET> using resbasicblock64 = resblock<basicblock, 64, bn_con, SUBNET>;
-    template<typename SUBNET> using resbottleneck512 = resblock<bottleneck, 512, bn_con, SUBNET>;
-    template<typename SUBNET> using resbottleneck256 = resblock<bottleneck, 256, bn_con, SUBNET>;
-    template<typename SUBNET> using resbottleneck128 = resblock<bottleneck, 128, bn_con, SUBNET>;
-    template<typename SUBNET> using resbottleneck64 = resblock<bottleneck, 64, bn_con, SUBNET>;
+    template<typename SUBNET> using resbasicblock512 = residual_block<residual, basicblock, 512, bn_con, SUBNET>;
+    template<typename SUBNET> using resbasicblock256 = residual_block<residual, basicblock, 256, bn_con, SUBNET>;
+    template<typename SUBNET> using resbasicblock128 = residual_block<residual, basicblock, 128, bn_con, SUBNET>;
+    template<typename SUBNET> using resbasicblock64 = residual_block<residual, basicblock, 64, bn_con, SUBNET>;
+    template<typename SUBNET> using resbottleneck512 = residual_block<residual, bottleneck, 512, bn_con, SUBNET>;
+    template<typename SUBNET> using resbottleneck256 = residual_block<residual, bottleneck, 256, bn_con, SUBNET>;
+    template<typename SUBNET> using resbottleneck128 = residual_block<residual, bottleneck, 128, bn_con, SUBNET>;
+    template<typename SUBNET> using resbottleneck64 = residual_block<residual, bottleneck, 64, bn_con, SUBNET>;
 
     // and the equivalent affine versions for inference
-    template<typename SUBNET> using aresbasicblock512 = resblock<basicblock, 512, affine, SUBNET>;
-    template<typename SUBNET> using aresbasicblock256 = resblock<basicblock, 256, affine, SUBNET>;
-    template<typename SUBNET> using aresbasicblock128 = resblock<basicblock, 128, affine, SUBNET>;
-    template<typename SUBNET> using aresbasicblock64 = resblock<basicblock, 64, affine, SUBNET>;
-    template<typename SUBNET> using aresbottleneck512 = resblock<bottleneck, 512, affine, SUBNET>;
-    template<typename SUBNET> using aresbottleneck256 = resblock<bottleneck, 256, affine, SUBNET>;
-    template<typename SUBNET> using aresbottleneck128 = resblock<bottleneck, 128, affine, SUBNET>;
-    template<typename SUBNET> using aresbottleneck64 = resblock<bottleneck, 64, affine, SUBNET>;
+    template<typename SUBNET> using aresbasicblock512 = residual_block<residual, basicblock, 512, affine, SUBNET>;
+    template<typename SUBNET> using aresbasicblock256 = residual_block<residual, basicblock, 256, affine, SUBNET>;
+    template<typename SUBNET> using aresbasicblock128 = residual_block<residual, basicblock, 128, affine, SUBNET>;
+    template<typename SUBNET> using aresbasicblock64 = residual_block<residual, basicblock, 64, affine, SUBNET>;
+    template<typename SUBNET> using aresbottleneck512 = residual_block<residual, bottleneck, 512, affine, SUBNET>;
+    template<typename SUBNET> using aresbottleneck256 = residual_block<residual, bottleneck, 256, affine, SUBNET>;
+    template<typename SUBNET> using aresbottleneck128 = residual_block<residual, bottleneck, 128, affine, SUBNET>;
+    template<typename SUBNET> using aresbottleneck64 = residual_block<residual, bottleneck, 64, affine, SUBNET>;
 }
